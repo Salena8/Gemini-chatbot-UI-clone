885465eafd66892d989a9a7d39a6fe41b42c8f65
Salena8 <salinachhetri7766@gamil.com>
2025-09-20 22:03:16 +0545

    Initial commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,58 @@
+import React, {useContext} from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets/assets'
+import { Context } from '../../Context/Context'
+
+
+const Main = () => {
+  const{onSent, recentPrompt, showResult, loading, resultData, input, setInput} = useContext(Context);
+
+  return (
+    <div className='main'>
+        <div className='nav'>
+            <pr>Gemini</pr>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+          <div className="greet">
+            <p><span>Hi, Dev</span></p>
+            <p>How can I help you?</p>
+          </div>
+          <div className="cards">
+            <div className="card">
+              <p>Suggest some beautiful place to see</p>
+              <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+              <p>Briefly summarize the concept:sustainable development</p>
+              <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+              <p>Brainstorm some team bonding activities</p>
+              <img src={assets.message_icon} alt="" />
+            </div>
+            <div className="card">
+              <p>Improve readability of following code</p>
+              <img src={assets.code_icon} alt="" />
+            </div>
+          </div>
+          <div className="main-bottom">
+            <div className="search-box">
+              <input onChange = {(e)=>setInput(e.target.value)} value={input} type='text' placeholder='Enter a prompt here' />
+              <div>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
+              </div>
+              
+            </div>
+            <p className='bottom-info'>
+                Gemini may display inaccurate info, including about people so double check its responses. Your privacy and Gemini Apps
+              </p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/config/gemini.js
@@ -0,0 +1,6 @@
+export default async function generateAnswer(){
+const response = await axios({
+    url:" ",
+    method:"post"
+  })
+}
